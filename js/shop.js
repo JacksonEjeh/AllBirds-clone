bc439042b74ee5fc89a56f9ead0f4dc7db19a63a
JacksonEjeh <gabbyjax7@gmail.com>
2024-07-17 11:42:46 +0100

    second version

---
+++ js/shop.js
@@ -0,0 +1,287 @@
+$(document).ready(function(){
+    $('.j_arrow').click(function(){
+        $('.j_topbar_text').toggle()
+        $('.j_topbar_text1').toggle()
+    })
+    // MEN
+    $('#mens').click(function(){
+        $('#mens').toggleClass('active')
+        $('#womens').removeClass('active')
+        $('#kids').removeClass('active')
+        $('#sustain').removeClass('active')
+        $('#sale').removeClass('active')
+        $('.j_mens_overlay').fadeToggle()
+        $('.j_womens_overlay').fadeOut()
+        $('.j_kids_overlay').fadeOut()
+        $('.j_sale_overlay').fadeOut()
+        $('.j_sustain_overlay').fadeOut()
+    })
+    $('.j_mens_overlay').click(function(){
+        $('.j_mens_overlay').fadeOut()
+        $('#mens').removeClass('active')
+
+    })
+    // WOMEN
+    $('#womens').click(function(){
+        $('#womens').toggleClass('active')
+        $('#mens').removeClass('active')
+        $('#kids').removeClass('active')
+        $('#sustain').removeClass('active')
+        $('#sale').removeClass('active')
+        $('.j_womens_overlay').fadeToggle()
+        $('.j_mens_overlay').fadeOut()
+        $('.j_kids_overlay').fadeOut()
+        $('.j_sustain_overlay').fadeOut()
+        $('.j_sale_overlay').fadeOut()
+    })
+    $('.j_womens_overlay').click(function(){
+        $('.j_womens_overlay').fadeOut()
+        $('#womens').removeClass('active')
+
+    })
+    // KIDS
+    $('#kids').click(function(){
+        $('#kids').toggleClass('active')
+        $('#mens').removeClass('active')
+        $('#womens').removeClass('active')
+        $('#sustain').removeClass('active')
+        $('#sale').removeClass('active')
+        $('.j_kids_overlay').fadeToggle()
+        $('.j_womens_overlay').fadeOut()
+        $('.j_mens_overlay').fadeOut()
+        $('.j_sale_overlay').fadeOut()
+        $('.j_sustain_overlay').fadeOut()
+    })
+    $('.j_kids_overlay').click(function(){
+        $('.j_kids_overlay').fadeOut()
+        $('#kids').removeClass('active')
+    })
+    // SUSTAIN
+    $('#sustain').click(function(){
+        $('#sustain').toggleClass('active')
+        $('#kids').removeClass('active')
+        $('#mens').removeClass('active')
+        $('#womens').removeClass('active')
+        $('#sale').removeClass('active')
+        $('.j_sustain_overlay').fadeToggle()
+        $('.j_kids_overlay').fadeOut()
+        $('.j_womens_overlay').fadeOut()
+        $('.j_mens_overlay').fadeOut()
+        $('.j_sale_overlay').fadeOut()
+    })
+    $('.j_sustain_overlay').click(function(){
+        $('.j_sustain_overlay').fadeOut()
+        $('#sustain').removeClass('active')
+    })
+    // SALE
+    $('#sale').click(function(){
+        $('#sale').toggleClass('active')
+        $('#sustain').removeClass('active')
+        $('#kids').removeClass('active')
+        $('#mens').removeClass('active')
+        $('#womens').removeClass('active')
+        $('.j_sale_overlay').fadeToggle()
+        $('.j_kids_overlay').fadeOut()
+        $('.j_womens_overlay').fadeOut()
+        $('.j_sustain_overlay').fadeOut()
+        $('.j_mens_overlay').fadeOut()
+    })
+    $('.j_sale_overlay').click(function(){
+        $('.j_sale_overlay').fadeOut()
+        $('#sale').removeClass('active')
+    })
+
+    //CART
+    $('.j_cart_logo').click(function(){
+        $('.j_cart_overlay').fadeToggle()
+        $('.j_cart_popup').toggleClass('slidein')
+        $('.j_kids_overlay').hide()
+        $('.j_womens_overlay').hide()
+        $('.j_sustain_overlay').hide()
+        $('.j_mens_overlay').hide()
+        $('.j_sale_overlay').hide()
+        $('#sale').removeClass('active')
+        $('#sustain').removeClass('active')
+        $('#kids').removeClass('active')
+        $('#mens').removeClass('active')
+        $('#womens').removeClass('active')
+    })
+    $('.j_cart_overlay').click(function(){
+        // $('.j_cart_overlay').fadeOut()
+    })
+    $('.j_close').click(function(){
+        $('.j_cart_popup').toggleClass('slidein')
+        $('.j_cart_overlay').fadeToggle()
+    })
+
+
+    $('#jBest').click(function(){
+        $('.jCheck1').toggleClass('jCheck_active')
+        $('.jbold1').toggleClass('jbold_active')
+        $('.jCheck2').removeClass('jCheck_active')
+        $('.jbold2').removeClass('jbold_active')
+        $('.jCheck3').removeClass('jCheck_active')
+        $('.jbold3').removeClass('jbold_active')
+        $('.jCheck4').removeClass('jCheck_active')
+        $('.jbold4').removeClass('jbold_active')
+        $('.jCheck5').removeClass('jCheck_active')
+        $('.jbold5').removeClass('jbold_active')
+    })
+    $('#jBest2').click(function(){
+        $('.jCheck5').removeClass('jCheck_active')
+        $('.jbold5').removeClass('jbold_active')
+        $('.jCheck4').removeClass('jCheck_active')
+        $('.jbold4').removeClass('jbold_active')
+        $('.jCheck1').removeClass('jCheck_active')
+        $('.jCheck3').removeClass('jCheck_active')
+        $('.jbold3').removeClass('jbold_active')
+        $('.jbold1').removeClass('jbold_active')
+        $('.jCheck2').toggleClass('jCheck_active')
+        $('.jbold2').toggleClass('jbold_active')
+
+    })
+    $('#jBest3').click(function(){
+        $('.jCheck3').toggleClass('jCheck_active')
+        $('.jbold3').toggleClass('jbold_active')
+        $('.jCheck1').removeClass('jCheck_active')
+        $('.jbold1').removeClass('jbold_active')
+        $('.jCheck2').removeClass('jCheck_active')
+        $('.jbold2').removeClass('jbold_active')
+        $('.jCheck4').removeClass('jCheck_active')
+        $('.jbold4').removeClass('jbold_active')
+        $('.jCheck5').removeClass('jCheck_active')
+        $('.jbold5').removeClass('jbold_active')
+    })
+    $('#jBest4').click(function(){
+        $('.jCheck4').toggleClass('jCheck_active')
+        $('.jbold4').toggleClass('jbold_active')
+        $('.jCheck3').removeClass('jCheck_active')
+        $('.jbold3').removeClass('jbold_active')
+        $('.jCheck1').removeClass('jCheck_active')
+        $('.jbold1').removeClass('jbold_active')
+        $('.jCheck2').removeClass('jCheck_active')
+        $('.jbold2').removeClass('jbold_active')
+        $('.jCheck5').removeClass('jCheck_active')
+        $('.jbold5').removeClass('jbold_active')
+    })
+    $('#jBest5').click(function(){
+        $('.jCheck5').toggleClass('jCheck_active')
+        $('.jbold5').toggleClass('jbold_active')
+        $('.jCheck3').removeClass('jCheck_active')
+        $('.jbold3').removeClass('jbold_active')
+        $('.jCheck1').removeClass('jCheck_active')
+        $('.jbold1').removeClass('jbold_active')
+        $('.jCheck2').removeClass('jCheck_active')
+        $('.jbold2').removeClass('jbold_active')
+        $('.jCheck4').removeClass('jCheck_active')
+        $('.jbold4').removeClass('jbold_active')
+    })
+    $('#jBest6').click(function(){
+        $('.jCheck6').toggleClass('jCheck_active')
+        $('.jbold6').toggleClass('jbold_active')
+    })
+    $('#jBest7').click(function(){
+        $('.jCheck7').toggleClass('jCheck_active')
+        $('.jbold7').toggleClass('jbold_active')
+    })
+    $('#jBest8').click(function(){
+        $('.jCheck8').toggleClass('jCheck_active')
+        $('.jbold8').toggleClass('jbold_active')
+    })
+    $('#jBest9').click(function(){
+        $('.jCheck9').toggleClass('jCheck_active')
+        $('.jbold9').toggleClass('jbold_active')
+    })
+
+
+    $('#j_1Img').click(function(){
+        $('#j_1Img').addClass('active2')
+        $('#j_2Img').removeClass('active2')
+        $('#j_3Img').removeClass('active2')
+        $('#j_4Img').removeClass('active2')
+        $('#j_5Img').removeClass('active2')
+        $('.j_1Img').fadeIn()
+        $('.j_2Img').fadeOut()
+        $('.j_3Img').fadeOut()
+        $('.j_4Img').fadeOut()
+        $('.j_5Img').fadeOut()
+    })
+    $('#j_2Img').click(function(){
+        $('#j_2Img').addClass('active2')
+        $('#j_1Img').removeClass('active2')
+        $('#j_3Img').removeClass('active2')
+        $('#j_4Img').removeClass('active2')
+        $('#j_5Img').removeClass('active2')
+        $('.j_2Img').fadeIn()
+        $('.j_3Img').fadeOut()
+        $('.j_4Img').fadeOut()
+        $('.j_5Img').fadeOut()
+    })
+    $('#j_3Img').click(function(){
+        $('#j_3Img').addClass('active2')
+        $('#j_1Img').removeClass('active2')
+        $('#j_2Img').removeClass('active2')
+        $('#j_4Img').removeClass('active2')
+        $('#j_5Img').removeClass('active2')
+        $('.j_3Img').fadeIn()
+        $('.j_2Img').fadeOut()
+        $('.j_4Img').fadeOut()
+        $('.j_5Img').fadeOut()
+    })
+    $('#j_4Img').click(function(){
+        $('#j_4Img').addClass('active2')
+        $('#j_1Img').removeClass('active2')
+        $('#j_3Img').removeClass('active2')
+        $('#j_2Img').removeClass('active2')
+        $('#j_5Img').removeClass('active2')
+        $('.j_4Img').fadeIn()
+        $('.j_2Img').fadeOut()
+        $('.j_3Img').fadeOut()
+        $('.j_5Img').fadeOut()
+    })
+    $('#j_5Img').click(function(){
+        $('#j_5Img').addClass('active2')
+        $('#j_1Img').removeClass('active2')
+        $('#j_3Img').removeClass('active2')
+        $('#j_4Img').removeClass('active2')
+        $('#j_2Img').removeClass('active2')
+        $('.j_5Img').fadeIn()
+        $('.j_2Img').fadeOut()
+        $('.j_3Img').fadeOut()
+        $('.j_4Img').fadeOut()
+    })
+
+    let count = 1
+    $('#increaseBtn').click(function(){
+        count ++;
+        $('#counter').html(count)
+    })
+    $('#decreaseBtn').click(function(){
+        count -= 1;
+        if (count <= 0) {
+            $('#counter').html(count = 1)
+        } else{
+            $('#counter').html(count)
+        }
+    })
+
+
+    $('.jAllBird').click(function(){
+        $('.jAllBirdHide').slideToggle()
+        $('.jdownArrow').toggle()
+        $('.jupArrow').toggleClass('opacity')
+    })
+    $('.jAllBird1').click(function(){
+        $('.jAllBirdHide1').slideToggle()
+        $('.jdownArrow1').toggle()
+        $('.jupArrow1').toggleClass('opacity1')
+    })
+    
+    $('.jShopItemImg').click(function(){
+        window.location.href = "product_page.html"
+    })
+    $('.jShopItemTitle').click(function(){
+        window.location.href = "product_page.html"
+    })
+
+})
